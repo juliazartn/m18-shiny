15c2a99d95ae627cd6469996f8869536fbab1e9b
juliazartn <julia.zartn@gmail.com>
2017-05-11 14:17:26 -0700

    In class exercise 1 complete

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,10 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/INFO 201/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.r")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildPlot(iris, 'Sepal.Length', 'Sepal.Width', 'Species', "Sepal Length")
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,17 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+library(plotly)
+
+BuildPlot <- function(df, my_x, my_y, my_title = "Title", my_xaxis = "X Title", my_yaxis = "Y Title") {
+  p <- ggplot(data = df) +
+        geom_point(mapping = aes(x = df[,my_x], y = df[,my_y])) + 
+        labs(title = my_title, x = my_xaxis, y = my_yaxis)
+  return (p)
+}
+
+
+plot <- BuildPlot(mtcars, 'cyl', 'wt')
+
+
